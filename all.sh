64656760bff53b8c00dc302897e6e97f4c39d890
KIMBtech <info@kimb-technologies.eu>
2019-09-28 13:13:34 +0200

    Docker Hub Token

--- all.sh
+++ all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
